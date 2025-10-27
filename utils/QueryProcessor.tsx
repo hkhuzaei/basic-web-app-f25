d76f4144acefb2be499e83ad4039d9233c07072f
Haneen <hkhuzaei@andrew.cmu.edu>
2025-10-27 08:51:41 +0000

    fixing name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,8 +7,8 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
-  if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+  if (query.toLowerCase().includes("What is your name?")) {
+    return "Haneen";
   }
 
   if (query.toLowerCase().includes("what is your andrew id")) {
